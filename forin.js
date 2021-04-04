550a7b710db50a31260df48e66a5f0eeeb22535d
olga20011 <olgaestherakello@gmail.com>
2021-04-04 14:08:21 +0300

    loops

---
+++ forin.js
@@ -0,0 +1,17 @@
+const names=["olga","esther","faith","mercy","ruth"]
+for (name of names){
+    console.log(name)
+}
+
+const string="code"
+for (let i of string){
+    console.log(i)
+}
+
+const x=["firstName:Angella","lastName:Vickie"]
+for (key in x) console.log(x[key]);
+
+const set=[1,2,3];
+for (let x in set){
+    console.log(x);
+}
